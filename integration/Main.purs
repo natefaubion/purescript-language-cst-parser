d6f55659942627bec8c3102102095e9367f254a4
Thomas Honeyman <admin@thomashoneyman.com>
2021-02-06 12:31:33 -0800

    foldFree -> runFree

--- integration/Main.purs
+++ integration/Main.purs
@@ -2,7 +2,7 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Free (foldFree)
+import Control.Monad.Free (runFree)
 import Data.Array (foldMap)
 import Data.Array as Array
 import Data.Either (Either(..), either)
@@ -110,4 +110,4 @@ parseModuleFromFile path = do
 
 parse :: TokenStream -> Either ParseError (Module Unit)
 parse tokenStream =
-  unwrap $ foldFree identity $ Parsing.runParserT tokenStream Parser.parseModule
+  runFree unwrap $ Parsing.runParserT tokenStream Parser.parseModule
